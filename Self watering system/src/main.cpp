7fd024aef9fdf0885f2039de75690a81440515ab
Scott Trimble <scotty1993@hotmail.co.uk>
2021-05-06 18:38:21 +0100

    Very first steps. This code just sets us GPIO4 as an ADC and takes a reading roughly every second. If this reading is above ~1.65V the onboard LED will turn on. It also returns the ADC reading over serial.

--- Self watering system/src/main.cpp
+++ Self watering system/src/main.cpp
@@ -1,9 +1,37 @@
+// This code uses the arduino framework.
 #include <Arduino.h>
 
+//Defines for pins
+#define ledPin 2
+#define moisturePin 4
+
+// Set up global variables
+int moistureVal = 0;
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // Setup serial
+  Serial.begin(9600);
+  // initialize and set up pins.
+  pinMode(ledPin, OUTPUT);
+  pinMode(moisturePin, INPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read the moisture and send the reading on serial
+  moistureVal = analogRead(moisturePin);
+  Serial.println(moistureVal);
+
+  // If the mositure reading is greater than 2048 (ADC value ranges from 0-4095 so this is about half)
+  // turn the LED on
+  if(moistureVal>2047){
+    digitalWrite(ledPin, HIGH);   // turn the LED on by setting pin to HIGH
+  }
+  else  // Else LED off
+  {
+    digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  }
+  // 1 second delay at the end of loop.
+  delay(1000);  
 }
